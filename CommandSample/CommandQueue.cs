3dd77c5ad25c096232b2a8f71a546c2e865be062
ahao214 <haochen214@163.com>
2020-03-25 22:29:29 +0800

    实现命令队列

---
+++ CommandSample/CommandQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSample
+{
+    class CommandQueue
+    {
+        //定义一个List来存储命令队列
+        private List<Command> commands = new List<Command>();
+
+        public void AddCommand(Command command)
+        {
+            commands.Add(command);
+        }
+
+        public void RemoveCommand(Command command)
+        {
+            commands.Remove(command);
+        }
+
+        //循环调用每一个命令对象的Execute()方法
+        public void Execute()
+        {
+            foreach (object command in commands)
+            {
+                ((Command)command).Execute();
+            }
+        }
+
+    }
+}
---
+++ CommandSample/Invoker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSample
+{
+    class Invoker
+    {
+        private CommandQueue commandQueue;  //维持一个CommandQueue对象的引用
+
+        //构造注入
+        public Invoker(CommandQueue commandQueue)
+        {
+            this.commandQueue = commandQueue;
+        }
+
+        //设置注入
+        public void SetCommandQueue(CommandQueue commandQueue)
+        {
+            this.commandQueue = commandQueue;
+        }
+
+        //调用CommandQueue类的Execute方法
+        public void Call()
+        {
+            commandQueue.Execute();
+        }
+
+    }
+}
