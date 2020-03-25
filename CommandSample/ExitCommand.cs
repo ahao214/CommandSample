1e52f158736d5185390150d2ea8aa3e748091ecb
ahao214 <haochen214@163.com>
2020-03-25 22:00:28 +0800

    具体命令类和接收者

---
+++ CommandSample/ExitCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSample
+{
+    /// <summary>
+    /// 退出命令类，充当具体命令类
+    /// </summary>
+    class ExitCommand:Command
+    {
+        private SystemExitClass seObj;  //维持对请求接收者的引用
+
+        public ExitCommand()
+        {
+            seObj = new SystemExitClass();
+        }
+
+        //命令执行方法，将调用请求接收者的业务方法
+        public override void Execute()
+        {
+            seObj.Exit();
+        }
+    }
+}
---
+++ CommandSample/SystemExitClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSample
+{
+    /// <summary>
+    /// 退出系统类模拟实现类，充当请求接收者
+    /// </summary>
+    class SystemExitClass
+    {
+        public void Exit()
+        {
+            Console.WriteLine("退出系统");
+        }
+    }
+}
