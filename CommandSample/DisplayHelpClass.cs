d81440a02611057949d095a34325a29e3205cce2
ahao214 <haochen214@163.com>
2020-03-25 22:04:04 +0800

    具体命令类和请求接收者

---
+++ CommandSample/DisplayHelpClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSample
+{
+    /// <summary>
+    /// 显示帮助文档模拟实现类，充当请求接收者
+    /// </summary>
+    class DisplayHelpClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("显示帮助文档");
+        }
+    }
+}
---
+++ CommandSample/HelpCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSample
+{
+    /// <summary>
+    /// 帮助命令类，充当具体命令类
+    /// </summary>
+    class HelpCommand:Command
+    {
+        private DisplayHelpClass hcObj;     //维持对请求接收者的引用
+        public HelpCommand()
+        {
+            hcObj = new DisplayHelpClass();
+        }
+
+        //命令执行方法，将调用请求接收者的业务方法
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
